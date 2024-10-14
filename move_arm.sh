27b5ce75b3790d973db44254a72594ec39576bb3
BaekYang <scarlet3090@gmail.com>
2024-10-14 20:40:09 +0900

    .

--- move_arm.sh
+++ move_arm.sh
@@ -1,2 +1,2 @@
-file_name=parallel
+file_name=robot_data_1
 python run_scenarioFile.py --file_name $file_name
--- run_scenarioFile.py
+++ run_scenarioFile.py
@@ -82,10 +82,10 @@ with open(file_path, 'r') as csvfile:
 
         data.append({k:float(v) for k, v in row.items()})
 
-
+arm.set_suction_cup(False)
 for point in data:
     angles = [point['joint1'], point['joint2'], point['joint3'], point['joint4'], point['joint5'], point['joint6']]
-    arm.set_servo_angle(angle=angles, speed=params.angle_speed, mvacc=params.angle_acc, wait=True, radius=0.0)
+    arm.set_servo_angle(angle=angles, speed=params.angle_speed, mvacc=params.angle_acc, wait=True, radius=100.0)
     # print(point)
 coordinates, angles = get_robot_state()
 print(coordinates, angles)

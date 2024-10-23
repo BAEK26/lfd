1238115339ef0287b1efc449725c351dd68b8088
BaekYang <168061530+BaekYang@users.noreply.github.com>
2024-10-23 21:40:21 +0900

    Update rlmodel.py

--- rl/rlmodel.py
+++ rl/rlmodel.py
@@ -1,6 +1,13 @@
-# PPO 모델 생성 및 학습
+from stable_baselines3 import PPO
+from env_setup import RobotEnv
+from stable_baselines3.common.envs import DummyVecEnv
+
+# 환경 설정
+env = DummyVecEnv([lambda: RobotEnv()])
+
+# PPO 모델 생성
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=50000)  # 학습 시간 조절
 
 # 학습된 모델 저장
 model.save("ppo_robot_lfd")

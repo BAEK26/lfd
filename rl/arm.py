1c004acfddce37214bf0e511ad8e8c4ba4bbdeca
BaekYang <168061530+BaekYang@users.noreply.github.com>
2024-10-23 22:14:03 +0900

    Update arm.py

--- rl/arm.py
+++ rl/arm.py
@@ -1,26 +1,11 @@
-import gym
-import numpy as np
-from ufactory import uArm
+from UFactoryLite6 import UFactoryLite6
 
-class RobotArm:
+class RobotArmController:
     def __init__(self):
-        # Lite6 초기화 (SDK 사용)
-        self.arm = uArm.UFactoryLite6()
-        self.arm.connect()  # 로봇 암 연결
-        self.arm.initialize()  # 초기화
-        self.state = np.zeros(6)  # 6축 로봇 암의 초기 상태 (관절 각도)
+        self.robot = UFactoryLite6()
 
-    def step(self, action):
-        # SDK를 사용해 관절 각도 조정
-        target_angles = np.clip(self.state + action, -180, 180)  # 각도 제한
-        self.arm.set_joint_positions(target_angles.tolist())
-        self.state = target_angles
-        return self.state
+    def move_arm(self, joint_positions):
+        self.robot.move_joints(joint_positions)
 
     def get_state(self):
-        # 로봇 암의 현재 관절 각도를 반환
-        current_angles = self.arm.get_joint_positions()
-        return np.array(current_angles)
-
-    def disconnect(self):
-        self.arm.disconnect()  # 로봇 암 연결 종료
+        return self.robot.get_joint_positions()

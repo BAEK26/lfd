a45f48d821692342521bd0a7a5400c6cef5c5f19
BaekYang <168061530+BaekYang@users.noreply.github.com>
2024-10-23 21:45:13 +0900

    Update simul.py

--- rl/simul.py
+++ rl/simul.py
@@ -1,8 +1,16 @@
-# 모델 불러오기 및 테스트
+from stable_baselines3 import PPO
+from env_setup import RobotEnv
+from stable_baselines3.common.envs import DummyVecEnv
+
+# 모델 불러오기
 model = PPO.load("ppo_robot_lfd")
 
+# 환경 설정
+env = DummyVecEnv([lambda: RobotEnv()])
+
+# 시뮬레이션 실행
 obs = env.reset()
 for _ in range(100):
-    action, _states = model.predict(obs)
+    action, _ = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render()

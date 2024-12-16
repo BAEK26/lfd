133081d7765863e845deec36dc1246f1c85ca472
BaekYang <168061530+BaekYang@users.noreply.github.com>
2024-12-16 18:07:03 +0900

    Update run2.py
    env2에 맞게 연결

--- ppo_xarm_tensorboard/run2.py
+++ ppo_xarm_tensorboard/run2.py
@@ -2,7 +2,7 @@ import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, BaseCallback
-from xarm_env import XArmEnv
+from env2 import XArmEnv
 import numpy as np
 from tqdm import tqdm
 

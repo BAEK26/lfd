c6eac67ea3b56f9376cc3dffdcdeaa417ffab566
Jeongeun Yang <168061530+BaekYang@users.noreply.github.com>
2025-02-28 16:45:57 +0900

    Update scale.ino
    보드레이트 늘림, 잡소리 제거

--- Arduino/scale.ino
+++ Arduino/scale.ino
@@ -16,19 +16,13 @@ const float CALIBRATION_FACTOR = -719.78350;
 bool tareButtonPressed = false;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-  Serial.println("HX711 무게 감지 예제 (고정된 보정값 사용)");
 
   // 고정된 보정값 설정
   scale.set_scale(CALIBRATION_FACTOR); 
   
-  // 영점 설정
-  Serial.println("초기 영점 설정 중...");
   scale.tare(); 
-  Serial.println("초기 영점 설정 완료");
-
   // Tare 버튼 핀 설정
   pinMode(TARE_BUTTON_PIN, INPUT_PULLUP); // 풀업 저항 사용
 }
@@ -49,12 +43,8 @@ void loop() {
   // 무게 출력
   if (scale.is_ready()) {
     float weight = scale.get_units(5); // 평균 5회 측정
-    Serial.print("측정된 무게: ");
     Serial.print(weight, 2); // 소수점 두 자리까지 표시
     Serial.println(" g");
-  } else {
-    Serial.println("HX711을 찾을 수 없습니다.");
-  }
-  
-  delay(500);
+  } 
+  delay(100);
 }

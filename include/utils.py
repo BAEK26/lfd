20fc6780269cd516437ae12adaa289af4f91d10e
Sumin Park <69071922+parksumin1017@users.noreply.github.com>
2025-03-07 15:16:27 +0900

    Revert "Update utils.py"
    This reverts commit 8c70895e2f534fe3cca0596a6cd40b6169986505.

--- include/utils.py
+++ include/utils.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
 
-from src.trajectory import Trajectory  # main.py에서 실행할 경우
+try:
+    from trajectory import Trajectory  # 직접 실행할 경우
+except ImportError:
+    from src.trajectory import Trajectory  # main.py에서 실행할 경우
 
 # 시각화
 class Visualize:
@@ -134,3 +137,10 @@ def random_near_endpoints(trajectory, option='end', random_rate=0.1):
             break
     
     return tuple(random_point)
+
+# 예제 코드
+if __name__ == "__main__":
+    
+    # CSV 불러와 Trajectory 객체 생성
+    traj = Trajectory.load_csv("test_sumin_a.csv")
+    Visualize.simulate(traj, frame_rate=10)
